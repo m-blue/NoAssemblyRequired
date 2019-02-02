3e2be4456c1a927ba8d91b86b88452d2052f4975
Mark Blue <bigblue_ii@yahoo.com>
2019-02-02 13:54:41 -0600

    Initial commit

---
+++ app/src/main/java/com/example/blue/noassemblyrequired/IkeaDresser.java
@@ -0,0 +1,13 @@
+package com.example.blue.noassemblyrequired;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IkeaDresser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ikea_dresser);
+    }
+}
