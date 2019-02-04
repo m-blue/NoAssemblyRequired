b26c4c790e7e5eb8f78e170a5d2afcd2bad7d040
Mark Blue <bigblue_ii@yahoo.com>
2019-02-03 20:17:30 -0600

    added btn code

--- app/src/main/java/com/example/blue/noassemblyrequired/MainActivity.java
+++ app/src/main/java/com/example/blue/noassemblyrequired/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.blue.noassemblyrequired;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnIkeaDresser = (Button)findViewById(R.id.btnIKEADresser);
+        Button btnBicycle = (Button)findViewById(R.id.btnBicycle);
+        Button btnWoodPatio = (Button)findViewById(R.id.btnWoodPatio);
+
+        btnIkeaDresser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,IkeaDresser.class));
+            }
+        });
+
+        btnBicycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Bicycle.class));
+            }
+        });
+
+        btnWoodPatio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,WoodPatio.class));
+            }
+        });
     }
 }
